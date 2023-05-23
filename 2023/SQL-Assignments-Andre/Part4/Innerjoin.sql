2324e825e3d8a3ee66bdeca510fdc8fd8f9f16ed
001164110 <44639490+INeedHelp01@users.noreply.github.com>
2023-05-23 15:35:02 +0930

    fixing shit

--- 2023/SQL-Assignments-Andre/Part4/Innerjoin.sql
+++ 2023/SQL-Assignments-Andre/Part4/Innerjoin.sql
@@ -3,13 +3,6 @@ use sams;
 select * from test
 where TestName = 'Engine Start and run up according to AFM procedures';
 
-select TestEvent.aircraftID, Technician.firstName, technician.lastName
-from TestEvent
-inner join testitemtestevent on testevent.testEventCode = testitemtestevent.testEventCode
-inner join techniciantestitemtestevent on testitemtestevent.testEventCode = techniciantestitemtestevent.testEventCode
-inner join technician on techniciantestitemtestevent.technicianID = technician.technicianID
-inner join testitem on technician.technicianID = testitem.technicianID
-where testItemName = 'Engine Start and run up according to AFM procedures' AND (testevent.dateStart between '2022-01-01' and '2022-01-31');
 
 SELECT TestEvent.aircraftID, Technician.firstName, technician.lastName, TestEvent.dateStart
 FROM TestItem
