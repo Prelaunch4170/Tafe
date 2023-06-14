2eef56a15ea559e6505a6ecdc1f0b0b8d22d2634
AndreAlexandrov <44639490+INeedHelp01@users.noreply.github.com>
2023-06-14 09:41:45 +0930

    origonate

---
+++ 2023/OrigonateApp/app/src/main/java/com/example/origonateapp/Userdetails.java
@@ -0,0 +1,14 @@
+package com.example.origonateapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Userdetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userdetails);
+    }
+}
