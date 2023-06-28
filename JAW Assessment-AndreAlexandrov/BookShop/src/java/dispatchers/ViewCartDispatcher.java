9ae989408dfb614d281bed980572369e5e69f0ae
AndreAlexandrov <44639490+INeedHelp01@users.noreply.github.com>
2023-06-28 19:35:40 +0930

    finished jaw

---
+++ JAW Assessment-AndreAlexandrov/BookShop/src/java/dispatchers/ViewCartDispatcher.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dispatchers;
+
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author dalevanh
+ */
+public class ViewCartDispatcher implements IDispatcher{
+
+    public String execute(HttpServletRequest request) {
+        String nextPage = "/jsp/cart.jsp";
+        HttpSession session = request.getSession();
+        Map cart = (Map) session.getAttribute("cart");
+        if (cart == null) {
+            nextPage = "/jsp/titles.jsp";
+            return nextPage;
+        }
+        return nextPage;
+    }
+}
