5cd72d0b6a56fb0a4b25f284abfdc167dc4cd03a
001164110 <44639490+INeedHelp01@users.noreply.github.com>
2023-08-24 13:32:09 +0930

    fuck

--- 2023/Semester 2/614/python/main.py
+++ 2023/Semester 2/614/python/main.py
@@ -8,20 +8,16 @@ import CallApi
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 
-fuel_dict = {2: "Unleaded", 3: "Diesel", 4: "LPG", 5: "Premium Unleaded 95", 6: "ULSD", 8: "Premium Unleaded 98",
-             11: "LRP", 12: "e10", 13: "Premium e5", 14: "Premium Diesel", 16: "Bio-Diesel 20", 19: "e85", 21: "OPAL",
-             22: "Compressed natural gas", 23: "Liquefied natural gas", 999: "e10/Unleaded",
-             1000: "Diesel/Premium Diesel"}
-
 client = Client(account_sid, auth_token)
-apiMessage = CallApi.Call_api()
+
+fuelAPIMessage = CallApi.Call_api()
 
 message = client.messages \
     .create(
-    body=apiMessage,
+    body=fuelAPIMessage,
     from_='+17068073673',
     to='+61410531436'
 )
 
-print(apiMessage)
+print(fuelAPIMessage)
 print(message.sid)
