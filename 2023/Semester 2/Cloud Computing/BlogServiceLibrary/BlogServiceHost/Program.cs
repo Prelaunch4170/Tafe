8443ba5d7324a34ca20562c152e0c5a611cc1686
001164110 <44639490+INeedHelp01@users.noreply.github.com>
2023-09-08 15:18:05 +0930

    IDK

---
+++ 2023/Semester 2/Cloud Computing/BlogServiceLibrary/BlogServiceHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using BlogServiceLibrary;
+
+namespace BlogServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+        }
+    }
+   
+}
