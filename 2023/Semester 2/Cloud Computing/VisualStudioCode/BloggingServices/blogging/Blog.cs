5ac1ff1075793861efbef30cb7018d3ce852df67
AndreAlexandrov <44639490+INeedHelp01@users.noreply.github.com>
2023-09-18 22:38:53 +0930

    ireally dont know

---
+++ 2023/Semester 2/Cloud Computing/VisualStudioCode/BloggingServices/blogging/Blog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace blogging
+{
+    [DataContract]
+    public class Blog
+    {
+        [DataMember]
+        public string CreatedBy;
+        [DataMember] 
+        public string DateCreated;
+        [DataMember] 
+        public string Name;
+        [DataMember]
+        public List<Post> Posts;
+
+    }
+}
---
+++ 2023/Semester 2/Cloud Computing/VisualStudioCode/BloggingServices/blogging/BloggingService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace blogging
+{
+    [ServiceBehavior (InstanceContextMode =InstanceContextMode.Single)]
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BloggingService" in both code and config file together.
+    public class BloggingService : IBloggingService
+    {
+        public List<Blog> Blogs = new List<Blog>();
+
+        public void AdddNewBlog(Blog thisblog)
+        {
+            Blogs.Add(thisblog);
+        }
+
+        public void AddPostToBlog(string blogName, Post postings)
+        {
+            var posts = GetAllPostsForBlog(blogName);
+            posts.Add(postings);
+        }
+
+        public List<Blog> GetAllBlogs()
+        {
+            return Blogs;
+        }
+
+        public List<Post> GetAllPostsForBlog(string blogName)
+        {
+            var blogs = Blogs.FirstOrDefault(b => b.Name == blogName);
+            if (blogs == null)
+            {
+                blogs.Posts = new List<Post>();
+            }
+            return blogs.Posts;
+        }
+
+
+    }
+}
