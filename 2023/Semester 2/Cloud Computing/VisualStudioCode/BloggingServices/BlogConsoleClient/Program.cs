1b05d9299fb2a4be55599cdeaf0b2744aa067135
AndreAlexandrov <44639490+INeedHelp01@users.noreply.github.com>
2023-09-18 23:04:51 +0930

    did session6

---
+++ 2023/Semester 2/Cloud Computing/VisualStudioCode/BloggingServices/BlogConsoleClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogConsoleClient.ServiceReference1;
+using blogging;
+
+namespace BlogConsoleClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BloggingServiceClient client = new BloggingServiceClient();
+
+            Blog blog2 = new Blog();
+            blog2.Name = "test";
+            blog2.CreatedBy = "Andre";
+            blog2.DateCreated =  DateTime.Today.ToString();
+            client.AdddNewBlog(blog2);
+
+            Blog blog1 = new Blog();
+            blog1.Name = "Samsons";
+            blog1.CreatedBy = "samson";
+            blog1.DateCreated = DateTime.Today.ToString();
+            client.AdddNewBlog(blog1);
+
+            foreach(var blog in client.GetAllBlogs())
+            {
+                Console.WriteLine(blog.Name + " "+ blog.CreatedBy + " " + blog.DateCreated);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2023/Semester 2/Cloud Computing/VisualStudioCode/BloggingServices/BlogServiceHost/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using blogging;
+
+
+namespace BlogServiceHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8733/blogging");
+
+            ServiceHost selfHost = new ServiceHost(typeof(BloggingService) ,baseAddress);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(IBloggingService), new WSHttpBinding(), "blogging.BloggingService");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                selfHost.Open();
+                Console.WriteLine("The Service is ready");
+                Console.WriteLine("Press <Enter> to terminate the service.");
+                Console.WriteLine("");
+                Console.ReadLine();
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
