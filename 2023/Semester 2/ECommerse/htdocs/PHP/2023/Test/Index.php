5ce88dafea3d16ece134ecc5ce658ebab2a2416d
001164110 <44639490+INeedHelp01@users.noreply.github.com>
2023-09-26 11:50:37 +0930

    a bunch of shit

--- 2023/Semester 2/ECommerse/htdocs/PHP/2023/Test/Index.php
+++ 2023/Semester 2/ECommerse/htdocs/PHP/2023/Test/Index.php
@@ -1,122 +1,17 @@
 <!DOCTYPE html>
-
 <html>
-
 <head>
-    <title>View Shopping cart Boiler</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="CSS/main.css">
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Welcome</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='/CSS/main.css'>
+
 </head>
-<?php
-$test = "number 1";
-
-?>
-
 <body>
-    <div class="topnav">
-        <div class="links">
-            <a class="active" href="#home">Home</a>
-            <a href="#news">News</a>
-            <a href="#contact">Contact</a>
-            <a href="#about">About</a>
-        </div>
-    </div>
+    <a href="phpPages/searchAll.php">
+    <button>Enter</button>
+    </a>
     
-    <div class="productView">
-        <div class="card">
-            <img src="img/testimage.png" alt="Denim Jeans" style="width:100%">
-            <h1>Tailored Jeans</h1>
-            <p class="price">$19.99</p>
-            <p>Some text about the jeans..</p>
-            <p>
-                <button name="View">View Product</button>
-            </p>
-        </div>
-        <div class="card">
-            <img src="img/testimage.png" alt="Denim Jeans" style="width:100%">
-            <h1>Tailored Jeans</h1>
-            <p class="price">$19.99</p>
-            <p><span><?php echo $test; ?></span></p>
-            <p>
-                <button name="View">View Product</button>
-            </p>
-        </div>
-        <div class="card">
-            <img src="img/testimage.png" alt="Denim Jeans" style="width:100%">
-            <h1>Tailored Jeans</h1>
-            <p class="price">$19.99</p>
-            <p><span><?php echo $test; ?></span></p>
-            <p>
-                <button name="View">View Product</button>
-            </p>
-        </div>
-        <div class="card">
-            <img src="img/testimage.png" alt="Denim Jeans" style="width:100%">
-            <h1>Tailored Jeans</h1>
-            <p class="price">$19.99</p>
-            <p><span><?php echo $test; ?></span></p>
-            <p>
-                <button name="View">View Product</button>
-            </p>
-        </div>
-        <div class="card">
-            <img src="img/testimage.png" alt="Denim Jeans" style="width:100%">
-            <h1>Tailored Jeans</h1>
-            <p class="price">$19.99</p>
-            <p><span><?php echo $test; ?></span></p>
-            <p>
-                <button name="View">View Product</button>
-            </p>
-        </div>
-        <div class="card">
-            <img src="img/testimage.png" alt="Denim Jeans" style="width:100%">
-            <h1>Tailored Jeans</h1>
-            <p class="price">$19.99</p>
-            <p><span><?php echo $test; ?></span></p>
-            <p>
-                <button name="View">View Product</button>
-            </p>
-        </div>
-        <div class="card">
-            <img src="img/testimage.png" alt="Denim Jeans" style="width:100%">
-            <h1>Tailored Jeans</h1>
-            <p class="price">$19.99</p>
-            <p><span><?php echo $test; ?></span></p>
-            <p>
-                <button name="View">View Product</button>
-            </p>
-        </div>
-        <div class="card">
-            <img src="img/testimage.png" alt="Denim Jeans" style="width:100%">
-            <h1>Tailored Jeans</h1>
-            <p class="price">$19.99</p>
-            <p><span><?php echo $test; ?></span></p>
-            <p>
-                <button name="View">View Product</button>
-            </p>
-        </div>
-        <div class="card">
-            <img src="img/testimage.png" alt="Denim Jeans" style="width:100%">
-            <h1>Tailored Jeans</h1>
-            <p class="price">$19.99</p>
-            <p><span><?php echo $test; ?></span></p>
-            <p>
-                <button name="View">View Product</button>
-            </p>
-        </div>
-
-    </div>
-
-
-    <script src="js/main.js" async defer></script>
-    <footer>
-        <div class="info">
-            <p>Author: Hege Refsnes</p>
-            <p>fun boys</p>
-        </div>
-    </footer>
 </body>
-
-
 </html>
---
+++ 2023/Semester 2/ECommerse/htdocs/PHP/2023/Test/phpPages/searchAll.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>View All</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../CSS/main.css">
+</head>
+<?php
+$test = "number 1";
+
+?>
+
+<body>
+    <div class="topnav">
+        <div class="links">
+            <a class="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    </div>
+
+    <div class="viewDiv">
+        <div class="sidePanel">
+            <label for="search">Search</label> 
+            <input name="search" type="text" size=10/>
+            <p>Testing the side panel</p>
+            <p>Testing the side panel</p>
+            <p>Testing the side panel</p>
+            <p>Testing the side panel</p>
+        </div>
+        <div class="productView">
+            <div class="card">
+                <img src="../img/testimage.png" alt="Denim Jeans" style="width:100%">
+                <h1>Tailored Jeans</h1>
+                <p class="price">$19.99</p>
+                <p>Some text about the jeans..</p>
+                <p>
+                    <button name="View">View Product</button>
+                </p>
+            </div>
+            <div class="card">
+                <img src="../img/testimage.png" alt="Denim Jeans" style="width:100%">
+                <h1>Tailored Jeans</h1>
+                <p class="price">$19.99</p>
+                <p><span><?php echo $test; ?></span></p>
+                <p>
+                    <button name="View">View Product</button>
+                </p>
+            </div>
+            <div class="card">
+                <img src="../img/testimage.png" alt="Denim Jeans" style="width:100%">
+                <h1>Tailored Jeans</h1>
+                <p class="price">$19.99</p>
+                <p><span><?php echo $test; ?></span></p>
+                <p>
+                    <button name="View">View Product</button>
+                </p>
+            </div>
+            <div class="card">
+                <img src="../img/testimage.png" alt="Denim Jeans" style="width:100%">
+                <h1>Tailored Jeans</h1>
+                <p class="price">$19.99</p>
+                <p><span><?php echo $test; ?></span></p>
+                <p>
+                    <button name="View">View Product</button>
+                </p>
+            </div>
+            <div class="card">
+                <img src="../img/testimage.png" alt="Denim Jeans" style="width:100%">
+                <h1>Tailored Jeans</h1>
+                <p class="price">$19.99</p>
+                <p><span><?php echo $test; ?></span></p>
+                <p>
+                    <button name="View">View Product</button>
+                </p>
+            </div>
+            <div class="card">
+                <img src="../img/testimage.png" alt="Denim Jeans" style="width:100%">
+                <h1>Tailored Jeans</h1>
+                <p class="price">$19.99</p>
+                <p><span><?php echo $test; ?></span></p>
+                <p>
+                    <button name="View">View Product</button>
+                </p>
+            </div>
+            <div class="card">
+                <img src="../img/testimage.png" alt="Denim Jeans" style="width:100%">
+                <h1>Tailored Jeans</h1>
+                <p class="price">$19.99</p>
+                <p><span><?php echo $test; ?></span></p>
+                <p>
+                    <button name="View">View Product</button>
+                </p>
+            </div>
+            <div class="card">
+                <img src="../img/testimage.png" alt="Denim Jeans" style="width:100%">
+                <h1>Tailored Jeans</h1>
+                <p class="price">$19.99</p>
+                <p><span><?php echo $test; ?></span></p>
+                <p>
+                    <button name="View">View Product</button>
+                </p>
+            </div>
+            <div class="card">
+                <img src="../img/testimage.png" alt="Denim Jeans" style="width:100%">
+                <h1>Tailored Jeans</h1>
+                <p class="price">$19.99</p>
+                <p><span><?php echo $test; ?></span></p>
+                <p>
+                    <button name="View">View Product</button>
+                </p>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+    <script src="../js/main.js" async defer></script>
+    <footer>
+        <div class="info">
+            <p>Author: Andre Alexandrov</p>
+            <p>I have no clue what im doing :)</p>
+            <p>All license to me</p>
+        </div>
+    </footer>
+</body>
+
+
+</html>
