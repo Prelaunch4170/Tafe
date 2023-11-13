c4a07d7fac78fc490ab1484927769bad0288f398
AndreAlexandrov <44639490+INeedHelp01@users.noreply.github.com>
2023-11-14 00:02:24 +1030

    asd

---
+++ Test/railRoad1.sav.lua
@@ -0,0 +1,54 @@
+function data()
+return {
+	["arrivaltracker.lua"] = {
+		data = {
+			__ug_gui_game_info_cargo = 1429,
+			__ug_gui_game_info_passenger = 390,
+		},
+		filterdata = {
+			__ug_gui_game_info_cargo = { },
+			__ug_gui_game_info_passenger = {
+				cargotype = "PASSENGERS",
+			},
+		},
+	},
+	["contexthelper.lua"] = {
+		openAtStart = false,
+	},
+	["gameinfo.lua"] = { },
+	["guidesystem.lua"] = {
+		active = true,
+		completedGuides = {
+			borrowLoan = true,
+			buildBusStop = true,
+			buildStations = true,
+			buildStreets = true,
+			buildTracks = true,
+			bulldozer = true,
+			companyFinances = true,
+			companyGrowth = true,
+			contoursButton = true,
+			gameSpeed = true,
+			hudIcons = true,
+			keyScroll = true,
+			landuseButton = true,
+			mousePan = true,
+			mouseRotate = true,
+			mouseWheel = true,
+			openLayers = true,
+			openStats = true,
+			openTownWindow = true,
+			rotateConstructions = true,
+			speedLimitsButton = true,
+			stationQuality = true,
+			trafficLayerButton = true,
+			unpause = true,
+		},
+		time = 6795.2000000048,
+		timeLastGuide = 6290.0000000036,
+		timeLastMissionWindowVisibilityChange = -999,
+		timeLastProposal = 6567.4000000041,
+	},
+	["selectortooltip.lua"] = { },
+}
+end
